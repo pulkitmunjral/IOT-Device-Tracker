5249634f637ba142639af2b508e37b4249d6d33e
qe13026445 <qe13026445@ldap.com>
2022-02-20 22:41:15 +0530

    static file issue

--- SmarterCodes/settings.py
+++ SmarterCodes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,3 +149,4 @@ EMAIL_HOST_PASSWORD = 'kyondssan@0406'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
