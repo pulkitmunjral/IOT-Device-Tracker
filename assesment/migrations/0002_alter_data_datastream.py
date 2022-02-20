0abed0e9a07d47745d91df518d55205c75bd2ed6
qe13026445 <qe13026445@ldap.com>
2022-02-21 00:07:07 +0530

    adding env veriable

--- assesment/migrations/0002_alter_data_datastream.py
+++ assesment/migrations/0002_alter_data_datastream.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='data',
             name='datastream',
             field=models.IntegerField(),
