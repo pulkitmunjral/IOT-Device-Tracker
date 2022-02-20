82abd81a856cbbf6ad2b6c7c69f36a8172c541b6
qe13026445 <qe13026445@ldap.com>
2022-02-20 22:11:19 +0530

    email issue fixed

--- SmarterCodes/settings.py
+++ SmarterCodes/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['email']
-EMAIL_HOST_PASSWORD = os.environ['email_pass']
+EMAIL_HOST_USER = 'uschat.cf@gmail.com'
+EMAIL_HOST_PASSWORD = 'kyondssan@0406'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
